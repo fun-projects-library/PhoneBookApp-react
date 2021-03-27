644657dc9d0e5228c007535ff70dfba41bf795b8
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-03-27 15:29:15 -0700

    first react implementations

--- react-applicaation/src/App.js
+++ react-applicaation/src/App.js
@@ -5,7 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+      <h1>Hello World</h1>
+
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +20,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
